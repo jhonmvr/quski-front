04b7b1c26e8da615b2b1e6191756fd9477edd5d7
developer-four <developer-four@relative-engine.com>
2020-05-15 19:33:42 -0500

    Antes de opciones de credito

--- src/app/views/pages/apps/cotizacion/list-cotizar/list-cotizar.component.ts
+++ src/app/views/pages/apps/cotizacion/list-cotizar/list-cotizar.component.ts
@@ -10,6 +10,7 @@ import { ReNoticeService } from '../../../../../core/services/re-notice.service'
 import { SubheaderService } from '../../../../../core/_base/layout';
 import { merge, tap } from 'rxjs/operators';
 import { AuthDialogComponent } from '../../../../../views/partials/custom/auth-dialog/auth-dialog.component';
+import { TbQoPrecioOro } from '../../../../../core/model/quski/TbQoPrecioOro';
 
 
 @Component({
@@ -22,52 +23,81 @@ export class ListCotizarComponent implements OnInit {
   loading;
   loadingSubject = new BehaviorSubject<boolean>(false);
 
-
-  nombreCliente = new FormControl('', []);
-  apellidoCliente = new FormControl('', []);
-  identificacionCliente = new FormControl('', []); 
+  cedula = new FormControl('', []);
   fechaNacimiento = new FormControl('', []);
-  estadoCivil = new FormControl('', []);
-  genero = new FormControl('', []);
+  nombresCompletos = new FormControl('', []);
+  edad = new FormControl('', []);
+  nacionalidad = new FormControl('', []);
+  movil = new FormControl('', []);
+  telefonoDomicilio = new FormControl('', []);
+  publicidad = new FormControl('', []);
+  correoElectronico = new FormControl('', []);
+  campania = new FormControl('', []);
+  aprobadoWebMupi = new FormControl('', []);
+  apellidoCliente = new FormControl('', []);
+  //OPCIONES PRECIO ORO
+  tipoOro = new FormControl('', []);
+  precio = new FormControl('', []);
+  precioEstimado = new FormControl('', []);
+  pesoNetoEstimado = new FormControl('', []);
+  accion = new FormControl('', []);
+  //OPCIONES DE CREDITO
+  plazo = new FormControl('', []);
+  montoPreaprobado = new FormControl('', []);
+  aRecibir = new FormControl('', []);
+  totalCostosNuevaOperacion = new FormControl('', []);
+  costoCustodia = new FormControl('', []);
+  costoTransporte = new FormControl('', []);
+  costoCredito = new FormControl('', []);
+  costoSeguro = new FormControl('', []);
+  costoResguardo = new FormControl('', []);
+  costoEstimado = new FormControl('', []);
+  valorCuota = new FormControl('', []);
+  gradoInteres = new FormControl('', []);
+  motivoDesestimiento = new FormControl('', []);
 
-  displayedColumns = ['accion', 'cedula',  'primerNombre', 'nivelEducacion', 'genero', 'estadoCivil',
-   'nacionalidad', 'edad', 'actividadEconomica',  'canalContacto'];
+  displayedColumns = ['orden', 'variable', 'valor'];
+  displayedColumnsI = ['accion', 'tipoOro', 'precio', 'pesoNetoEstimado'];
+  displayedColumnsC = ['plazo', 'montoPreAprobado', 'aRecibir', 'totalCostosOperacion', 'costoCustodia', 'costoTransporte', 'costoCredito', 'costoSeguro', 'costoResguardo', 'costoEstimado', 'valorCuota'];
+
   /**Obligatorio paginacion */
   p = new Page();
-  dataSource:MatTableDataSource<TbMiCliente>=new MatTableDataSource<TbMiCliente>();
-  @ViewChild(MatPaginator, { static: true }) 
+  dataSource: MatTableDataSource<TbMiCliente> = new MatTableDataSource<TbMiCliente>();
+  dataSourceTipoPrecio: MatTableDataSource<TbQoPrecioOro> = new MatTableDataSource<TbQoPrecioOro>();
+  dataSourceCredito = new MatTableDataSource<any>();
+  @ViewChild(MatPaginator, { static: true })
   paginator: MatPaginator;
   totalResults: number;
   pageSize = 5;
   currentPage;
 
   /**Obligatorio ordenamiento */
-  @ViewChild('sort1', {static: true}) sort: MatSort;
-  
-  
+  @ViewChild('sort1', { static: true }) sort: MatSort;
+
+
 
   constructor(public titulo: TituloContratoService,
     private clienteService: ClienteService,
     private sinNoticeService: ReNoticeService,
     private subheaderService: SubheaderService,
-    private noticeService:ReNoticeService,
-		public dialog: MatDialog) {
-      this.clienteService.setParameter();
-     }
+    private noticeService: ReNoticeService,
+    public dialog: MatDialog) {
+    this.clienteService.setParameter();
+  }
 
   ngOnInit() {
     //this.titulo.setNotice("GESTION DE CLIENTES")
     this.loading = this.loadingSubject.asObservable();
     // Set title to page breadCrumbs
-    this.subheaderService.setTitle('Gestión Cotizaciones');
+    this.subheaderService.setTitle('Cotización');
     this.initiateTablePaginator();
     //Se ejecuta cuando se hace click en el ordenamiento en el mattable
     this.sort.sortChange.subscribe(() => {
-      console.log("sort changed "  );
+      console.log("sort changed ");
       this.initiateTablePaginator();
       this.buscar()
     });
-    
+
   }
 
   /**
@@ -75,16 +105,17 @@ export class ListCotizarComponent implements OnInit {
    */
   initiateTablePaginator(): void {
     this.dataSource = new MatTableDataSource();
+    this.dataSourceTipoPrecio = new MatTableDataSource();
     this.paginator.pageSize = 5;
     this.paginator.pageIndex = 0;
     this.totalResults = 0;
     this.dataSource.paginator = this.paginator;
   }
 
-   /**
-   * Obligatorio Paginacion: Obtiene el objeto paginacion a utilizar
-   */
-  getPaginacion(ordenarPor: string, tipoOrden: string, paginado: string,pagina): Page {
+  /**
+  * Obligatorio Paginacion: Obtiene el objeto paginacion a utilizar
+  */
+  getPaginacion(ordenarPor: string, tipoOrden: string, paginado: string, pagina): Page {
     const p = new Page();
     p.pageNumber = pagina;
     p.pageSize = this.paginator.pageSize;
@@ -95,64 +126,64 @@ export class ListCotizarComponent implements OnInit {
     return p;
   }
 
-  
-   /**
-   * Obligatorio Paginacion: Ejecuta la busqueda cuando se ejecuta los botones del paginador
-   */
+
+  /**
+  * Obligatorio Paginacion: Ejecuta la busqueda cuando se ejecuta los botones del paginador
+  */
   paged() {
-    this.p=this.getPaginacion(this.sort.active, this.sort.direction, 'Y',this.paginator.pageIndex)
+    this.p = this.getPaginacion(this.sort.active, this.sort.direction, 'Y', this.paginator.pageIndex)
     this.submit();
   }
 
-  
-   /**
-   * Obligatorio Paginacion: Obtiene el objeto paginacion a utilizar
-   */
+
+  /**
+  * Obligatorio Paginacion: Obtiene el objeto paginacion a utilizar
+  */
   buscar() {
     this.initiateTablePaginator();
-    this.p=this.getPaginacion(this.sort.active, this.sort.direction, 'Y',0);
+    this.p = this.getPaginacion(this.sort.active, this.sort.direction, 'Y', 0);
     this.submit();
   }
 
 
   submit() {
     //console.log("====> paged: " + JSON.stringify( this.p ));
-    this.loadingSubject.next(true);
-    this.dataSource = null;
-    this.clienteService.findClienteByParams(this.p, this.identificacionCliente.value, this.nombreCliente.value, this.apellidoCliente.value
-      ,null,null,null,null,null,null,null,null,null).subscribe((data: any) => {
-      this.loadingSubject.next(false);
-      //console.log("====> datos: " + JSON.stringify( data ));
-      if (data.list) {
-       
-        this.totalResults = data.totalResults;
-        this.dataSource = new MatTableDataSource<TbMiCliente>(data.list);
-        //this.dataSource.paginator=this.paginator;
-        this.sinNoticeService.setNotice("INFORMACION CARGADA CORRECTAMENTE", 'info');
-      } else {
-        this.sinNoticeService.setNotice("NO SE ENCONTRAR REGISTROS", 'success');
-      }
-    }, error => {
-      this.loadingSubject.next(false);
-      if(  error.error ){
-				this.noticeService.setNotice(error.error.codError + ' - ' + error.error.msgError  , 'error');
-			} else if(  error.statusText && error.status==401 ){
-				this.dialog.open(AuthDialogComponent, {
-					data: {
-						mensaje:"Error " + error.statusText + " - " + error.message
-					}
-				});
-			} else {
-				this.noticeService.setNotice("Error al cargar las notificaciones o alertas", 'error');
-			}
-    }
-    );
+    /* this.loadingSubject.next(true);
+     this.dataSource = null;
+     this.clienteService.findClienteByParams(this.p, this.cedula.value, this.nombresCompletos.value, this.apellidoCliente.value
+       ,null,null,null,null,null,null,null,null,null).subscribe((data: any) => {
+       this.loadingSubject.next(false);
+       //console.log("====> datos: " + JSON.stringify( data ));
+       if (data.list) {
+        
+         this.totalResults = data.totalResults;
+         this.dataSource = new MatTableDataSource<TbMiCliente>(data.list);
+         //this.dataSource.paginator=this.paginator;
+         this.sinNoticeService.setNotice("INFORMACION CARGADA CORRECTAMENTE", 'info');
+       } else {
+         this.sinNoticeService.setNotice("NO SE ENCONTRAR REGISTROS", 'success');
+       }
+     }, error => {
+       this.loadingSubject.next(false);
+       if(  error.error ){
+         this.noticeService.setNotice(error.error.codError + ' - ' + error.error.msgError  , 'error');
+       } else if(  error.statusText && error.status==401 ){
+         this.dialog.open(AuthDialogComponent, {
+           data: {
+             mensaje:"Error " + error.statusText + " - " + error.message
+           }
+         });
+       } else {
+         this.noticeService.setNotice("Error al cargar las notificaciones o alertas", 'error');
+       }
+     }
+     );*/
   }
 
   editarUsuario() {
     [{
       path: 'add/:id',
-    
+
     }]
 
   }
