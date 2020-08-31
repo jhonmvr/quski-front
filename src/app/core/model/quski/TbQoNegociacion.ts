cf26c398fcd876da202613c2cfd5151b34e73d05
developer-four <developer-four@relative-engine.com>
2020-08-31 11:03:31 -0500

    Cambios riesgo acumulado y Excepcion cliente

--- src/app/core/model/quski/TbQoNegociacion.ts
+++ src/app/core/model/quski/TbQoNegociacion.ts
@@ -9,23 +9,25 @@ import { TbQoCliente } from "./TbQoCliente";
 
 export class TbQoNegociacion {
     id: number;
-    asesorResponsable : string
-    codigoOperacion : string
+    asesorResponsable: string
+    codigoOperacion: string
     estado: string;
     fechaActualizacion: Date;
     fechaCreacion: Date;
-    idAsesorResponsable : number
-    procesoActualNegociacion : string
-    estadoNegociacion : string
-    tipoNegociacion : string
-    tbQoCreditoNegociacions : TbQoCreditoNegociacion[]
-    tbQoDocumentoHabilitantes : null
-    tbQoCliente : TbQoCliente
-    tbQoVariablesCrediticias : TbQoVariablesCrediticia [];
-    constructor(){
-       this.tbQoCliente = new TbQoCliente();
-       this.tbQoCreditoNegociacions  = new Array<TbQoCreditoNegociacion>();
-       this.tbQoVariablesCrediticias = new Array<TbQoVariablesCrediticia>();
+    idAsesorResponsable: number;
+    procesoActual: string
+    situacion: string
+    tipo: string
+    estadoNegociacion: string
+    tipoNegociacion: string
+    tbQoCreditoNegociacions: TbQoCreditoNegociacion[]
+    tbQoDocumentoHabilitantes: null
+    tbQoCliente: TbQoCliente
+    tbQoVariablesCrediticias: TbQoVariablesCrediticia[];
+    constructor() {
+        this.tbQoCliente = new TbQoCliente();
+        this.tbQoCreditoNegociacions = new Array<TbQoCreditoNegociacion>();
+        this.tbQoVariablesCrediticias = new Array<TbQoVariablesCrediticia>();
 
     }
 
