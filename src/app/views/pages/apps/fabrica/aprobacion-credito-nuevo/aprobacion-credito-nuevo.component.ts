a4b8a53654491847d1d182e5795461238494b83b
developer-four <developer-four@relative-engine.com>
2020-10-06 10:30:54 -0500

    cambios en aprobacion credito nuevo

--- src/app/views/pages/apps/fabrica/aprobacion-credito-nuevo/aprobacion-credito-nuevo.component.ts
+++ src/app/views/pages/apps/fabrica/aprobacion-credito-nuevo/aprobacion-credito-nuevo.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material';
 import { TbQoPatrimonioCliente } from '../../../../../core/model/quski/TbQoPatrimonioCliente';
 import { TbQoIngresoEgresoCliente } from '../../../../../core/model/quski/TbQoIngresoEgresoCliente';
 import { TbReferencia } from '../../../../../core/model/quski/TbReferencia';
+import { TbQoTasacion } from '../../../../../core/model/quski/TbQoTasacion';
 
 @Component({
   selector: 'app-aprobacion-credito-nuevo',
@@ -23,6 +24,10 @@ export class AprobacionCreditoNuevoComponent implements OnInit {
   // TABLA DE REFERENCIAS PERSONALES
   displayedColumnsReferencia = ['Accion', 'N', 'NombresCompletos', 'Parentesco', 'Direccion', 'TelefonoMovil', 'TelefonoFijo'];
   dataSourceReferencia = new MatTableDataSource<TbReferencia>();
+
+  // TABLA TASACION
+  displayedColumnsTasacion = ['NumeroPiezas', 'TipoOro', 'TipoJoya', 'Estado', 'Descripcion', 'PesoBruto', 'DescuentoPiedra', 'DescuentoSuelda', 'PesoNeto', 'ValorAvaluo', 'ValorComercial', 'ValorRealizacion', 'ValorOro'];
+  dataSourceTasacion = new MatTableDataSource<TbQoTasacion>();
 
   constructor() { }
 
